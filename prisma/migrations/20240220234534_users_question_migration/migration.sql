193fe9516bb878a94140da472f4d5e7ec3efb9c0
luiz bueno <luiz504@hotmail.com>
2024-02-20 20:55:27 -0300

    feat: route create-account

--- prisma/migrations/20240220234534_users_question_migration/migration.sql
+++ prisma/migrations/20240220234534_users_question_migration/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "possword" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,32 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private readonly prisma: PrismaService) {}
+  @Post()
+  @HttpCode(201)
+  async handle(@Body() body: any) { //eslint-disable-line 
+    const { name, email, password } = body
+
+    const useWithSameEmail = await this.prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (useWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
+    }
+
+    await this.prisma.user.create({
+      data: { email, password, name },
+    })
+  }
+}
