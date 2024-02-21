f02cf50d6bc37ce346c7da91ee31814d6c3749a3
luiz bueno <luiz504@hotmail.com>
2024-02-21 08:16:29 -0300

    feat: autheticate route

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,21 +1,51 @@
-import { Controller, Post } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
 
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation.pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
 
-// const createAccountBodySchema = z.object({
-//   email: z.string().email(),
-//   password: z.string().min(6),
-// })
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
 
-// type CreateAccountBody = z.infer<typeof createAccountBodySchema>
+type AuthenticateBody = z.infer<typeof authenticateBodySchema>
 @Controller('/sessions')
 export class AuthenticateController {
-  constructor(private readonly jwt: JwtService) {}
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+
   @Post()
-  //   @HttpCode(201)
-  //   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
-    async handle() { //eslint-disable-line 
-    const token = this.jwt.sign({ sub: 'user-id' })
-    return token
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBody) {
+    const { email, password } = body
+
+    const user = await this.prisma.user.findUnique({ where: { email } })
+
+    if (!user) {
+      throw new UnauthorizedException('User credentials does not match.')
+    }
+
+    const isPasswordValid = await compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentials does not match.')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return {
+      access_token: accessToken,
+    }
   }
 }
