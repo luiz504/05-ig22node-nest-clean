85483576ac746d1cd4b7a2af40655f90b7d10a95
luiz bueno <luiz504@hotmail.com>
2024-02-24 21:31:47 -0300

    test: Question/student persisted factories

--- src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug.controller.e2e-spec.ts
@@ -1,46 +1,43 @@
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
-import { expect } from 'vitest'
-import request from 'supertest'
 import { JwtService } from '@nestjs/jwt'
+import request from 'supertest'
 
-import { PrismaService } from '~/infra/database/prisma/prisma.service'
 import { AppModule } from '~/infra/app.module'
+import { DatabaseModule } from '~/infra/database/database.module'
+
+import { StudentFactory } from 'test/factories/make-student'
+import { QuestionFactory } from 'test/factories/make-question'
 
 describe('Get Question by Slug (e2e)', () => {
   let app: INestApplication
-  let prisma: PrismaService
   let jwt: JwtService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get(PrismaService)
+
     jwt = moduleRef.get(JwtService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
     await app.init()
   })
   test('[GET] /questions/:slug', async () => {
     // Prepare
-    const user = await prisma.user.create({
-      data: {
-        name: 'John',
-        email: 'john@email.com',
-        password: '123456',
-      },
-    })
-    const accessToken = jwt.sign({ sub: user.id })
+    const user = await studentFactory.makePrismaStudent()
+    const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    await prisma.question.create({
-      data: {
-        title: 'Question 01',
-        content: 'Question Content',
-        slug: 'question-01',
-        authorId: user.id,
-      },
+    questionFactory.makePrismaQuestion({
+      title: 'Question 01',
+      authorId: user.id,
     })
+
     // Actions
     const response = await request(app.getHttpServer())
       .get('/questions/question-01')
--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,9 +1,12 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '~/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '~/infra/database/prisma/mappers/prisma-question-mapper'
+import { PrismaService } from '~/infra/database/prisma/prisma.service'
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
@@ -20,4 +23,20 @@ export function makeQuestion(
   )
 
   return newQuestion
+}
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {},
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPersistance(question),
+    })
+
+    return question
+  }
 }
