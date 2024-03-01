7754d3123399729737f2dca01bc87304ca9eba83
luiz bueno <luiz504@hotmail.com>
2024-02-29 21:34:44 -0300

    feat: value-object CommentWithAuthor creation

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,6 +1,7 @@
 import { PaginationParams } from '~/core/repositories/pagination-params'
 
 import { QuestionComment } from '~/domain/forum/enterprise/entities/question-comment'
+import { CommentWithAuthor } from '~/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>
@@ -8,6 +9,11 @@ export abstract class QuestionCommentsRepository {
     questionId: string,
     params: PaginationParams,
   ): Promise<QuestionComment[]>
+
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
 
   abstract create(questionComment: QuestionComment): Promise<void>
   abstract delete(questionComment: QuestionComment): Promise<void>
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,12 +1,18 @@
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+
 import { DomainEvents } from '~/core/events/domain-events'
 import { PaginationParams } from '~/core/repositories/pagination-params'
+
 import { QuestionCommentsRepository } from '~/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '~/domain/forum/enterprise/entities/question-comment'
+import { CommentWithAuthor } from '~/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   public items: QuestionComment[] = []
+
+  constructor(private studentRepository: InMemoryStudentsRepository) {}
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment)
@@ -30,6 +36,36 @@ export class InMemoryQuestionCommentsRepository
       .slice((page - 1) * 20, page * 20)
 
     return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ) {
+    const questionCommentsWithAuthor = this.items
+
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentRepository.items.find((item) =>
+          item.id.equals(comment.authorId),
+        )
+        if (!author) {
+          throw new Error(
+            `Author with Id "${comment.authorId.toString()}" does not exist`,
+          )
+        }
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: comment.authorId,
+          authorName: author.name,
+        })
+      })
+
+    return questionCommentsWithAuthor
   }
 
   async delete(questionComment: QuestionComment) {
